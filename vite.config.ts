05ea4134b74caf3276372c35751d9bf6384257e8
Rodolfo Silva <rodolfo.diego.gomes@gmail.com>
2025-12-22 09:35:31 -0400

    Habilita PWA (instalável) com Vite PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(() => ({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "pwa-icon.svg"],
+      manifest: {
+        name: "Orfanato NIB",
+        short_name: "Orfanato NIB",
+        description: "Orfanato NIB",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/pwa-icon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any maskable"
+          }
+        ]
+      }
+    })
+  ],
   envDir: "env",
   resolve: {
     alias: {
