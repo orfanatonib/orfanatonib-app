b31ba1946b6be67368af74f5ce04165e47c63a28
Rodolfo Silva <rodolfo.diego.gomes@gmail.com>
2026-01-11 11:15:09 -0400

    fix

--- src/features/feature-flags/types.ts
+++ src/features/feature-flags/types.ts
@@ -1,5 +1,5 @@
 export enum FeatureFlagKeys {
     SHELTER_ADDRESS = 'shelter-address',
-    SHELTER_MANAGEMENT = 'sshelter-management',
+    SHELTER_MANAGEMENT = 'shelter-management',
     SHELTER_PAGELAS = 'shelter-pagelas',
 }
